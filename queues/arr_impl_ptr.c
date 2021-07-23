4f255d5c38e36a60c4fde876dd75ae2910ca6207
Garima Goel <garima.kkr1@gmail.com>
2021-07-23 11:21:10 +0530

    C program for implementation of queue using array, but here pointer to structure 'queue' is used

---
+++ queues/arr_impl_ptr.c
@@ -0,0 +1,64 @@
+// C program for implementation of queue using array, but here pointer to structure "queue" is used
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct queue
+{
+    int size;
+    int *arr;
+    int rear;
+    int front;
+} * q1;
+
+int isFull()
+{
+    return q1->rear == q1->size - 1 ? 1 : 0;
+}
+
+int isEmpty()
+{
+    return q1->rear == q1->front ? 1 : 0;
+}
+
+void enqueue(int x)
+{
+    if (isFull())
+        printf("Queue is full\n");
+    else
+        q1->arr[++q1->rear] = x;
+}
+
+int dequeue()
+{
+    if (isEmpty())
+    {
+        printf("Queue is empty\n");
+        return 0;
+    }
+    else
+        return q1->arr[++q1->front];
+}
+
+void display()
+{
+    for (int i = q1->front + 1; i <= q1->rear; i++)
+        printf("%d ", q1->arr[i]);
+}
+
+int main()
+{
+    q1 = (struct queue*)malloc(sizeof(struct queue));
+    q1->size = 8;
+    q1->arr = (int *)malloc(q1->size * sizeof(int));
+    q1->rear = -1;
+    q1->front = -1;
+
+    for (int i = 1; i <= 8; i++)
+        enqueue(i);
+
+    for (int i = 0; i < 3; i++)
+        dequeue();
+
+    display();
+}
